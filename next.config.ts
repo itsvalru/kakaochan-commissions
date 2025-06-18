aaa1b1d2c003297d2fbde6c425a7c921a193f723
itsvalru <79265002+itsvalru@users.noreply.github.com>
2025-06-18 13:49:38 +0000

    Initial Setup & Supabase

---
+++ next.config.ts
@@ -0,0 +1,11 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  content: [
+  "./src/**/*.{js,ts,jsx,tsx}",
+  "./app/**/*.{js,ts,jsx,tsx}",
+]
+
+};
+
+export default nextConfig;
